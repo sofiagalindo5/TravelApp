3afa8329d2e2189392e98ff652ed72b7cbc63492
sofiagalindol <sgalindo@sccs.edu.bo>
2026-05-24 19:52:56 -0400

    build config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-701u$wm!807kl)2=_t4b^79*sd5zap9v_a&6eljm&mv82#%pjs'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-701u$wm!807kl)2=_t4b^79*sd5zap9v_a&6eljm&mv82#%pjs')
 
 GOOGLE_FORM_WEBHOOK_SECRET = "supersecret123"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"] //change
+ALLOWED_HOSTS = ALLOWED_HOSTS = ["TravelJunkie.onrender.com", "localhost", "127.0.0.1"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
